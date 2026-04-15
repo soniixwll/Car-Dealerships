1cb0d488b361f9c6bf1190f498b9d61d34feedd3
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-15 19:53:32 +0300

    Fixing database and adding testdrive visits

--- autohub_bd/autohub/settings.py
+++ autohub_bd/autohub/settings.py
@@ -79,17 +79,20 @@ DATABASES = {
 AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ),
-    'DEFAULT_FILTER_BACKENDS': (
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
-    ),
+    ],
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 12,
 }
--- autohub_bd/bookings/views.py
+++ autohub_bd/bookings/views.py
@@ -1,16 +1,17 @@
-from rest_framework import viewsets, permissions, decorators, response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, decorators, permissions, response
 from .models import TestDriveBooking
 from .serializers import BookingSerializer
 
 
 class BookingViewSet(viewsets.ModelViewSet):
     serializer_class = BookingSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = [IsAuthenticated]
     http_method_names = ['get', 'post', 'delete']
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_admin:
+        if user.is_staff:
             return TestDriveBooking.objects.select_related('user', 'car', 'dealership').all()
         return TestDriveBooking.objects.filter(user=user).select_related('car', 'dealership')
 
